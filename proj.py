5965358401acdf5e3ac5f699b7f8bc82a55e01c3
Allie09 <122203495+Allie09@users.noreply.github.com>
2023-04-14 15:29:51 +0500

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from tkinter import *
 from datetime import datetime
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+
 df = pd.read_csv('Courses.csv')
 
 def Graph_building(df):
@@ -84,24 +89,20 @@ def optimized_timetable(all_timetables, graph): # This function basically check 
     
     return optimized_timetable
 
-
-def main(): # This is the function that we have to turn into a form or any app
-    courses_dict={1:"DSA",2:"Modernity",3:"Calc",4:"DM",5:"Bio"}
-    num_of_course=int(input("PLEASE ENTER THE NUMBER OF COURSES YOU WANT TO SELECT \n"))
+def main(courses_req,option): # This is the function that we have to turn into a form or any app
     courses=[]
     course_preference=[]
-    print("Please enter your courses one by one")
-    for i in range(num_of_course):
-        course_name=int(input("DSA => 1 , Modernity => 2 , Calc => 3 , DM => 4 , Bio => 5 \n"))
-        courses.append(courses_dict[course_name])
-        course_no=input("IF YOU WANT A SPECIFIC SECTION OF ANY COURSES PLEASE ENTER THE SECTION NUMBER: \n")
-        if course_no!="": course_preference.append((courses_dict[int(course_name)],int(course_no)))
-    optimized=input("DO YOU WANT BEST POSSIBLE OPTION? TRUE OR FALSE \n")
+    for course in courses_req:
+        if courses_req[course]==False:
+            continue
+        elif courses_req[course]!=None:
+            course_preference.append((course,courses_req[course]))
+        courses.append(course)
     graph=Graph_building(df)
     current_timetable = {}
     all_timetables = []
     generate_timetables(graph, courses, current_timetable, all_timetables)
-    if optimized==True :
+    if int(option)==1 :
         filtered_timetable=(optimized_timetable(all_timetables,graph))
         timeTable([filtered_timetable])
     else:
@@ -110,7 +111,7 @@ def main(): # This is the function that we have to turn into a form or any app
             all_preferences_satisfied = True
             for preference in course_preference:
                 course_name = preference[0]
-                course_no = preference[1]
+                course_no = df.loc[df['Instructor'] == preference[1], "Course No"].values[0]
                 if timetable[course_name] == int(course_no):
                     continue
                 else:
@@ -124,4 +125,117 @@ def main(): # This is the function that we have to turn into a form or any app
             print("No timetables found that include all preferences")
         else:
             timeTable(new_timetables)
-#main()
+courses={}
+option=""
+def Gen_timetable():
+    global courses,option
+    Optimized = accept_var.get()
+    firstname = first_name_entry.get()
+    lastname = last_name_entry.get()
+    ID=ID_entry.get()
+    DSA=DSA_combobox.get()
+    Modernity=Mordernity_combobox.get()
+    DM=DM_combobox.get()
+    Calculus=Calculus_combobox.get()
+    BioScience=BioScience_combobox.get()
+
+    if not(firstname and lastname and ID):
+        tkinter.messagebox.showwarning(title="Error", message="First name, last name, and Student ID are required.")
+    elif not DSA:
+        tkinter.messagebox.showwarning(title="Error", message="Select your DSA Instructor. Select 'Not to be enrolled' if you don't want to be enrolled in this course")
+    elif not DM:
+        tkinter.messagebox.showwarning(title="Error", message="Select your DM Instructor. Select 'Not to be enrolled' if you don't want to be enrolled in this course")
+    elif not Calculus:
+        tkinter.messagebox.showwarning(title="Error", message="Select your Calculus Instructor. Select 'Not to be enrolled' if you don't want to be enrolled in this course")
+    elif not Modernity:
+        tkinter.messagebox.showwarning(title="Error", message="Select your Modernity Instructor. Select 'Not to be enrolled' if you don't want to be enrolled in this course")
+    elif not BioScience:
+        tkinter.messagebox.showwarning(title="Error", message="Select your BioScience Instructor. Select 'Not to be enrolled' if you don't want to be enrolled in this course")
+
+    Courses_Required={'DSA':DSA,'DM':DM,'Calculus':Calculus,'Modernity':Modernity,'BioScience':BioScience}
+    for course in Courses_Required:
+        if Courses_Required[course]=='Not to be enrolled':
+            Courses_Required[course]=False
+        if Courses_Required[course]=='Any Instructor':
+            Courses_Required[course]=None
+    window.destroy()
+    courses=Courses_Required
+    option=Optimized
+    # return courses
+    
+##USE 'COURSES REQUIRED' FOR PROCESSING IN MAIN
+    
+###FRONT END
+window = tkinter.Tk()
+window.title("Data Entry Form")
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+# Saving User Info
+user_info_frame =tkinter.LabelFrame(frame, text="Student Information")
+user_info_frame.grid(row= 0, column=0, padx=20, pady=10)
+
+first_name_label = tkinter.Label(user_info_frame, text="First Name")
+first_name_label.grid(row=0, column=0)
+last_name_label = tkinter.Label(user_info_frame, text="Last Name")
+last_name_label.grid(row=0, column=1)
+Id_label = tkinter.Label(user_info_frame, text="Student ID")
+Id_label.grid(row=0, column=2)
+
+first_name_entry = tkinter.Entry(user_info_frame)
+last_name_entry = tkinter.Entry(user_info_frame)
+ID_entry = tkinter.Entry(user_info_frame)
+first_name_entry.grid(row=1, column=0)
+last_name_entry.grid(row=1, column=1)
+ID_entry.grid(row=1, column=2)
+
+for widget in user_info_frame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+# Saving Course Info
+courses_frame = tkinter.LabelFrame(frame)
+courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
+
+DSA_label = tkinter.Label(courses_frame, text="DSA")
+DSA_combobox = ttk.Combobox(courses_frame, values=['Not to be enrolled','M.Salman','F.Saleem','Any Instructor'])
+DSA_label.grid(row=0, column=0)
+DSA_combobox.grid(row=0, column=1)
+
+DM_label = tkinter.Label(courses_frame, text="DM")
+DM_combobox = ttk.Combobox(courses_frame, values=['Not to be enrolled','R.Ragib','A.Zafar','Any Instructor'])
+DM_label.grid(row=1, column=0)
+DM_combobox.grid(row=1, column=1)
+
+Calculus_label = tkinter.Label(courses_frame, text="Calculus II")
+Calculus_combobox = ttk.Combobox(courses_frame, values=['Not to be enrolled','S.Rana','Y.Kerai','Any Instructor'])
+Calculus_label.grid(row=2, column=0)
+Calculus_combobox.grid(row=2, column=1)
+
+Mordernity_label = tkinter.Label(courses_frame, text="Mordernity")
+Mordernity_combobox = ttk.Combobox(courses_frame, values=['Not to be enrolled','M.Patel','H.Habib','Any Instructor'])
+Mordernity_label.grid(row=3, column=0)
+Mordernity_combobox.grid(row=3, column=1)
+
+BioScience_label = tkinter.Label(courses_frame, text="BioScience")
+BioScience_combobox = ttk.Combobox(courses_frame, values=['Not to be enrolled','A.Nasir','J.Samad','Any Instructor'])
+BioScience_label.grid(row=4, column=0)
+BioScience_combobox.grid(row=4, column=1)
+
+
+for widget in courses_frame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+terms_frame = tkinter.LabelFrame(frame,)
+terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+
+accept_var = tkinter.StringVar(value=False)
+terms_check = tkinter.Checkbutton(terms_frame, text= "I want the TimeTable to be optimized",variable=accept_var, onvalue=True, offvalue=False)
+terms_check.grid(row=0, column=0)
+
+# Button
+button = tkinter.Button(frame, text="Generate Timetable", command= Gen_timetable)
+button.grid(row=3, column=0, padx=20, pady=10)
+ 
+window.mainloop()
+main(courses,option)
